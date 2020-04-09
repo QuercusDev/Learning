2787be48f8bfea1b36e1245562c01b5629a6a1ce
QuercusDev <63300993+QuercusDev@users.noreply.github.com>
2020-04-09 12:15:57 +0100

    Minimum solved

---
+++ minimum in an array/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        double[] a = {6.1, 10.0, 6.0, 7.3, 8.4, 7.2};
+        int i = 0;
+        int j = i + 1;
+
+        for(j = i + 1; j <= a.length - 1; j++) {
+            if (a[i] > a[j]) {
+                i = j;
+            }
+        }
+
+        System.out.println(a[i]);
+    }
+}
